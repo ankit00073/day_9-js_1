56cdb001b6ca6398e051b1fd449a48f83b33eb84
ankit00073 <64557746+ankit00073@users.noreply.github.com>
2023-08-26 13:54:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code here
+    const textInput = document.getElementById("textInput");
+    const addButton = document.getElementById("addButton");
+    const contentParagraph = document.getElementById("content");
+
+    addButton.addEventListener("click", () => {
+        const newText = textInput.value;
+
+        if (newText.trim() !== "") {
+            const newLine = document.createElement("br");
+            const newContent = document.createTextNode(newText);
+            contentParagraph.appendChild(newContent);
+            contentParagraph.appendChild(newLine);
+            textInput.value = "";
+        }
+    });
+});
